7de5dbd0536b58ba19b471e9b7de278f100065f5
Raphaella souza <raphaellaguerra@gmail.com>
2021-12-08 09:53:08 -0300

    @itzRapha [Task: classe Categoria com getters e setters]

---
+++ src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/model/Categoria.java
@@ -0,0 +1,82 @@
+package com.ProjetoGenFarmacia.ProjetoFarmacia.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size(max=40)
+	private String nomeCategoria;
+	
+	@NotBlank
+	@Size(min = 10, max = 60)
+	private String descricao;
+	
+	
+	@ManyToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Prduto> produtos = new ArrayList<>();
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public List<Prduto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Prduto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	
+	
+
+}
