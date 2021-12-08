b21ba5690655f34183344902cdce6e2f7949eb95
Gustavo Galli <gustavosgalli@gmail.com>
2021-12-08 10:21:46 -0300

    @gustavogalli [Task: criando classe ProdutoController e interface ProdutoRepository]

--- src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/controller/ProdutoController.java
+++ src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/controller/ProdutoController.java
@@ -1,5 +1,54 @@
 package com.ProjetoGenFarmacia.ProjetoFarmacia.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ProjetoGenFarmacia.ProjetoFarmacia.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin("*")
 public class ProdutoController {
+
+	private ProdutoRepository repository;
+
+	@GetMapping("/all")
+	public List<Produto> getAll() {
+		return repository.findAll();
+	}
+
+	@GetMapping("/id/{id}")
+	public Produto findById(@PathVariable long id) {
+		return repository.getById(id);
+	}
+
+	@GetMapping("descricao/{descricao}")
+	public List<Produto> findByDescricaoContainingIgnoreCase(@PathVariable String descricao) {
+		return repository.findByDescricaoContainingIgnoreCase(descricao);
+	}
+
+	@PostMapping("/save")
+	public Produto post(@RequestBody Produto produto) {
+		return repository.save(produto);
+	}
+
+	@PutMapping("/update")
+	public Produto put(@RequestBody Produto produto) {
+		return repository.save(produto);
+	}
+
+	@DeleteMapping("delete/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 
 }
--- src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/repository/ProdutoRepository.java
+++ src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/repository/ProdutoRepository.java
@@ -1,5 +1,13 @@
 package com.ProjetoGenFarmacia.ProjetoFarmacia.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findByDescricaoContainingIgnoreCase(String descricao);
 
 }
