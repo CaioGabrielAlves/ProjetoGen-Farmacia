13f28e17399e5174e4565b80482f0cadb3118178
CaioGabrielAlves <caio0401.ca@gmail.com>
2021-12-08 10:28:45 -0300

    @CaioGabrielAlves [Task: criando Model Produto]

--- src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/model/Produto.java
+++ src/main/java/com/ProjetoGenFarmacia/ProjetoFarmacia/model/Produto.java
@@ -1,5 +1,51 @@
-package com.ProjetoGenFarmacia.ProjetoFarmacia.model;
 
-public class Produto {
+@NotNull
+private double preco;
+
+@NotNull
+private long categoria;
+
+public long getId() {
+	return id;
+}
+
+public String getNomedomedicamento() {
+	return nomedomedicamento;
+}
+
+public void setNomedomedicamento(String nomedomedicamento) {
+	this.nomedomedicamento = nomedomedicamento;
+}
+
+public String getDescricao() {
+	return descricao;
+}
+
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+
+public double getPreco() {
+	return preco;
+}
+
+public void setPreco(double preco) {
+	this.preco = preco;
+}
+
+public long getCategoria() {
+	return categoria;
+}
+
+public void setCategoria(long categoria) {
+	this.categoria = categoria;
+}
+
+public void setId(long id) {
+	this.id = id;
+}
+	
+
 
 }
+
